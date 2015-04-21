3bca29607a1405a987017c86a0638f181eca986d
Malcolm <crummynz@gmail.com>
2015-04-20 17:46:36 -0700

    Layout calculation now occurs just once for items (like it should). Drag and highlight works correctly (though deselection does not). Regression: Text now does not appear.

--- app/src/main/java/com/malcolmcrum/draglauncher/DragView.java
+++ app/src/main/java/com/malcolmcrum/draglauncher/DragView.java
@@ -2,8 +2,6 @@ package com.malcolmcrum.draglauncher;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.support.annotation.NonNull;
@@ -26,21 +24,37 @@ public class DragView extends View {
     public DragView(Context context, DragMenuItem menuRoot) {
         super(context);
         menu = menuRoot;
-}
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        if (changed) {
+            int width = right - left;
+            int height = bottom - top;
+
+            layoutMenuItem(menu, width/2, height/2);
+        }
+    }
+
+    private void layoutMenuItem(DragMenuItem item, int x, int y) {
+        item.area = new Rect(x - item.getSize() / 2, y - item.getSize() / 2, x + item.getSize() / 2, y + item.getSize() / 2);
+
+        if (item.north != null) layoutMenuItem(item.north, x, y - item.getSpacing());
+        if (item.east != null) layoutMenuItem(item.east, x + item.getSpacing(), y);
+        if (item.south != null) layoutMenuItem(item.south, x, y + item.getSpacing());
+        if (item.west != null) layoutMenuItem(item.west, x - item.getSpacing(), y);
+    }
 
     @Override
     public void onDraw(Canvas canvas) {
-
-        int width = this.getWidth();
-        int height = this.getHeight();
-        center = new Point(width/2, height/2); // TODO: make this work out of the draw loop
-        drawItem(canvas, center.x, center.y, menu);
+        drawItem(canvas, menu);
     }
 
     @Override
     public boolean onTouchEvent(@NonNull MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
                 DragMenuItem selectedItem = itemAtCoords(event.getX(), event.getY(), menu);
                 if (selectedItem != null) {
                     selectedItem.select();
@@ -48,6 +62,7 @@ public class DragView extends View {
                 }
                 break;
             case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
                 menu.deselect();
                 invalidate();
                 break;
@@ -56,26 +71,39 @@ public class DragView extends View {
     }
 
     private DragMenuItem itemAtCoords(float x, float y, DragMenuItem root) {
-        if (rectContainsPoint(root.area, x, y)) {
-            return root;
-        }
-        else {
+        // Not my finest work.
+        // TODO: Combine this and probably rectContainsPoint into a nicer recursive function.
+        if (root == null) {
             return null;
+        } else if (rectContainsPoint(root.area, x, y)) {
+            return root;
+        } else if (root.isSelected()) {
+            DragMenuItem north = itemAtCoords(x, y, root.north);
+            DragMenuItem east = itemAtCoords(x, y, root.east);
+            DragMenuItem south = itemAtCoords(x, y, root.south);
+            DragMenuItem west = itemAtCoords(x, y, root.west);
+            if (north != null) return north;
+            if (east != null) return east;
+            if (south != null) return south;
+            if (west != null) return west;
         }
+        return null;
+
     }
 
     private boolean rectContainsPoint(Rect rect, float x, float y) {
         return (x > rect.left) && (x < rect.right) && (y > rect.top) && (y < rect.bottom);
     }
 
-    private void drawItem(Canvas canvas, int x, int y, DragMenuItem item) {
-        item.area = new Rect(x - item.getSize() / 2, y - item.getSize() / 2, x + item.getSize() / 2, y + item.getSize() / 2);
+    private void drawItem(Canvas canvas, DragMenuItem item) {
         canvas.drawRect(item.area, item.getBackgroundPaint());
-        canvas.drawText(item.label, x, y, item.getTextPaint());
+        //canvas.drawText(item.label, item.getTextPaint());
 
-        if (item.north != null) drawItem(canvas, x, y - item.getSpacing(), item.north);
-        if (item.east != null) drawItem(canvas, x + item.getSpacing(), y, item.east);
-        if (item.south != null) drawItem(canvas, x, y + item.getSpacing(), item.south);
-        if (item.west != null) drawItem(canvas, x - item.getSpacing(), y, item.west);
+        if (item.isSelected()) {
+            if (item.north != null) drawItem(canvas, item.north);
+            if (item.east != null) drawItem(canvas, item.east);
+            if (item.south != null) drawItem(canvas, item.south);
+            if (item.west != null) drawItem(canvas, item.west);
+        }
     }
 }
